1aa4bec5359c2a696ac25f8bc1bf5d14da14c115
paulojorgers <pjorgers07@gmail.com>
2025-02-07 20:13:55 -0300

    Atualizações na estrutura hero e adição de caminho nos links

--- script.js
+++ script.js
@@ -23,3 +23,13 @@ function closeModal() {
 }
 
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute("href"));
+        window.scrollTo({
+            top: target.offsetTop - 80, // Ajuste para não cobrir o título
+            behavior: "smooth"
+        });
+    });
+});
